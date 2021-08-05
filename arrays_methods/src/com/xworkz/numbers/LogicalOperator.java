8b49467dc9f62f3c2ff565a236590b61426149f8
rakshitha126 <nrakshitha661@gmail.com>
2021-08-05 19:40:38 +0530

    program based on Eclipse_Methods

---
+++ arrays_methods/src/com/xworkz/numbers/LogicalOperator.java
@@ -0,0 +1,19 @@
+package com.xworkz.numbers;
+
+public class LogicalOperator {
+	public static void main(String args[])
+	{
+		System.out.println("Program to illustrate logical operator");
+		boolean x = true , y = false;
+		
+		boolean result = x && y;
+		System.out.println(" x && y : " + result);
+		System.out.println(" x && y : " + (x && y));
+		
+		System.out.println(" x && y : " + (x && y));
+		
+		System.out.println(" x || y : " + (x || y));
+		
+		
+	}
+}
